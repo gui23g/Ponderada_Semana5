ed775d906ea95722995cf1fd05dfdb865347cd02
gui23g <guilherme.carvalho@sou.inteli.edu.br>
2024-03-07 09:17:17 -0300

    Update exercícios

---
+++ src/Questão1.js
@@ -0,0 +1,16 @@
+let numero =1;
+while(numero <=10){
+    if(numero % 2 ===0){
+        console.log(numero);
+    }
+    numero++;
+}
+
+//para provar que o primeiro numero e 1, podemos pedir para exibir os numeros impares
+let numero1 =1;
+while(numero1 <=10){
+    if(numero1 % 2 !=0){
+        console.log(numero1);
+    }
+    numero1++;
+}
